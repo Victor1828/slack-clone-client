52759f5fc2929f76fae006c78ccdb314edb72e1d
Victor Vega <victorvega1828@gmail.com>
2020-07-07 00:21:43 -0500

    :construction: Adding validations for register form

--- src/components/RegisterForm/index.js
+++ src/components/RegisterForm/index.js
@@ -1,48 +1,97 @@
 import React, { useState } from 'react'
-import { Button, Form, Container, Header } from 'semantic-ui-react'
+import { Button, Form, Container, Header, Input } from 'semantic-ui-react'
 import { gql } from 'apollo-boost'
 import { useMutation } from '@apollo/react-hooks'
+import { useHistory } from 'react-router-dom'
+
+const { Field } = Form
 
 const REGISTER_USER = gql`
   mutation($username: String!, $email: String!, $password: String!) {
     registerUser(username: $username, email: $email, password: $password) {
-      id
-      username
-      email
+      success
+      user {
+        id
+        username
+        email
+      }
+      errors {
+        path
+        message
+      }
     }
   }
 `
 
 const RegisterForm = () => {
   const [formInputValues, setFormInputValues] = useState({})
-  const [registerUser, { data }] = useMutation(REGISTER_USER)
+  const [formErrors, setFormErrors] = useState({})
+  const [registerUser] = useMutation(REGISTER_USER)
+  const history = useHistory()
+
+  const checkForErrors = data => {
+    const { registerUser: register } = data
+    const errors = {}
+
+    if (register.success) return
+
+    register.errors.forEach(error => {
+      errors[error.path] = {
+        content: error.message,
+        pointing: 'below',
+      }
+    })
+    setFormErrors(errors)
+  }
 
   const handleChange = event => {
     const { value, name } = event.target
     setFormInputValues({ ...formInputValues, [name]: value })
   }
 
-  const handleSubmit = () => {
-    registerUser({ variables: formInputValues })
-    console.log(data)
+  const handleSubmit = async () => {
+    try {
+      const { data } = await registerUser({ variables: formInputValues })
+      checkForErrors(data)
+      if (Object.keys(formErrors).length || !data.registerUser.success) return
+      history.push('/')
+    } catch (error) {
+      console.error(error)
+    }
   }
 
   return (
     <Container>
       <Header as="h2">Register</Header>
       <Form onSubmit={handleSubmit}>
-        <Form.Field>
-          <label>Username</label>
-          <input onChange={handleChange} name="username" placeholder="Username" />
-        </Form.Field>
-        <Form.Field>
-          <label>Email</label>
-          <input onChange={handleChange} name="email" placeholder="Email" />
-        </Form.Field>
-        <Form.Field>
-          <label>Passoword</label>
-          <input onChange={handleChange} name="password" placeholder="Password" />
-        </Form.Field>
+        <Field
+          control={Input}
+          label="Username"
+          placeholder="Username"
+          onChange={handleChange}
+          name="username"
+          error={formErrors.username}
+          required
+        />
+        <Field
+          control={Input}
+          label="Email"
+          placeholder="Email"
+          onChange={handleChange}
+          name="email"
+          error={formErrors.email}
+          required
+        />
+        <Field
+          control={Input}
+          type="password"
+          label="Password"
+          placeholder="Password"
+          onChange={handleChange}
+          name="password"
+          error={formErrors.password}
+          required
+        />
         <Button type="submit">Submit</Button>
       </Form>
     </Container>
